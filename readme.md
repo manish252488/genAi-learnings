ef1c1200763bce8c91a4f958dd59bad959967f4b
manish2524 <manish.s@rocketfuel.inc>
2025-12-25 16:46:02 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+llm provides multiple prompt templates 
+
+basic promt template
+chat promt template - used for chat bot promt template
---
+++ src/basicPromptTemplates.ts
@@ -0,0 +1,32 @@
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { config } from "dotenv";
+config();
+function generatePrompt(language: string) {
+    const promptTemplate = new PromptTemplate({
+        template: "Who created {language} and what is it?",
+        inputVariables: ["language"]
+    });
+    return promptTemplate.format({ language, words: 50 });
+}
+
+async function gemini() {
+    const promptTemplate = new PromptTemplate({
+        template: "Who created {language} and what is it?",
+        inputVariables: ["language"],
+    });
+    const model = new ChatGoogleGenerativeAI({
+        model: "gemini-2.5-flash",
+        apiKey: process.env.GOOGLE_API_KEY,
+
+    });
+    const outputParser = new StringOutputParser();
+    const chain = promptTemplate.pipe(model).pipe(outputParser);
+    const answer = await chain.invoke({
+        language: 'Node.js',
+    });
+    console.log('answer --> ', answer);
+}
+
+gemini();
